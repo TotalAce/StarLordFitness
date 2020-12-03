06d44e95f506f4bc1603afe5321d9c4074a02b02
Jishllg Laptop <jishllg@gmail.com>
2020-12-03 11:22:05 -0600

    Basic formatting cleanup for pre-login pages.

--- client/src/pages/signup.js
+++ client/src/pages/signup.js
@@ -1,18 +1,50 @@
-import React from "react";
-import "./style.css";
+import React, { useState } from "react";
 
 function Signup() {
+    const [userName, setUserName] = useState("");
+    const [password, setPassword] = useState("");
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [email, setEmail] = useState("");
+
+    const handleSubmit = (evt) => {
+        evt.preventDefault();
+        alert(`Login; User: ${userName}; Password: ${password}`)
+    }
+
     return (
-        <form>
-            <label for="email">First Name:</label><br/>
-            <input type="text" id="fname" name="fname"/><br/>
-            <label for="password">Last Name:</label><br/>
-            <input type="text" id="lname" name="lname"/>
+        <form onSubmit={handleSubmit}>
+            <label for="firstName">First Name:</label><br/>
+            <input 
+                type="text" 
+                value={firstName}
+                onChange={event => setFirstName(event.target.value)}
+                /><br/>
+            <label for="lastName">Last Name:</label><br/>
+            <input 
+                type="text" 
+                value={lastName}
+                onChange={event => setLastName(event.target.value)}
+                /><br/>
             <label for="email">Email:</label><br/>
-            <input type="text" id="email"/><br/>
+            <input 
+                type="text" 
+                value={email}
+                onChange={event => setEmail(event.target.value)}
+                /><br/>
+            <label for="username">Username:</label><br/>
+            <input 
+                type="text" 
+                value={userName}
+                onChange={event => setUserName(event.target.value)}
+                /><br/>
             <label for="password">Password:</label><br/>
-            <input type="text" id="password"/>
-            <input type="submit" value="signup"/>
+            <input 
+                type="text" 
+                value={password}
+                onChange={event => setPassword(event.target.value)}
+                /><br/>
+            <input type="submit" value="Log In"/>
         </form>
     );
 }
