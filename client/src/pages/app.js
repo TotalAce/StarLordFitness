3ae451e93e9c45c817d2750942389c83d5f28b2c
Shuelong Moua <moua_shuelong@yahoo.com>
2020-12-04 08:05:30 -0600

    imported all pages, and added routes

--- client/src/pages/app.js
+++ client/src/pages/app.js
@@ -1,29 +1,62 @@
 import React from "react";
 import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Link
-  } from "react-router-dom";
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import "./style.css";
 import Login from "./login.js";
 import Signup from "./signup.js";
 import Landing from "./landing.js";
+import Client from "./clientside/client";
+import ClientCalender from "./clientside/clientcalender";
+import ClientHome from "./clientside/clienthome";
+import ClientProfile from "./clientside/clientprofile";
+import Trainer from "./trainerside/trainer";
+import TrainerCalender from "./trainerside/trainercalender";
+import TrainerProfile from "./trainerside/trainerprofile";
+import TrainerSearch from "./trainerside/trainersearch";
+
 
 function App() {
   return (
     <Router>
-        <Switch>
-            <Route path="/login">
-                <Login />
-            </Route>
-            <Route path="/signup">
-                <Signup />
-            </Route>
-            <Route path="/">
-                <Landing />
-            </Route>
-        </Switch>
+      <Switch>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/signup">
+          <Signup />
+        </Route>
+        <Route path="/">
+          <Landing />
+        </Route>
+        <Route path="/client">
+          <Client />
+        </Route>
+        <Route path="/clientcalender">
+          <ClientCalender/>
+        </Route>
+        <Route path="/clienthome">
+          <ClientHome />
+        </Route>
+        <Route path="/clientprofile">
+          <ClientProfile />
+        </Route>
+        <Route path="/trainer">
+          <Trainer />
+        </Route>
+        <Route path="/trainercalender">
+          <TrainerCalender />
+        </Route>
+        <Route path="/trainerProfile">
+          <TrainerProfile />
+        </Route>
+        <Route path="/trainersearch">
+          <TrainerSearch />
+        </Route>
+      </Switch>
     </Router>
   );
 }
