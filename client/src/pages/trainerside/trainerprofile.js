2e2fa40c17be15cf6c8ef1d4dc46b7f3e7fa66e7
Shuelong Moua <moua_shuelong@yahoo.com>
2020-12-04 13:10:42 -0600

    created template for trainer profile

--- client/src/pages/trainerside/trainerprofile.js
+++ client/src/pages/trainerside/trainerprofile.js
@@ -1,12 +1,24 @@
 import React from "react";
+import NavBar from "../../components/Navbar";
 
 function TrainerProfile() {
 
 
     return (
-        <>
-            <h1>TrainerProfile</h1>
-        </>
+        <div className="container">
+            <br></br>
+            <NavBar />
+            <br></br>
+            <h2 className="d-flex justify-content-center"> Trainer Name</h2>
+            <br></br>
+            <div className="img-container d-flex justify-content-center">
+                <p>Insert Trainer Image</p>
+            </div>
+            <br></br>
+            <div className="d-flex justify-content-center">
+                <p>Trainer Summary</p>
+            </div>
+        </div>
     )
 }
 
