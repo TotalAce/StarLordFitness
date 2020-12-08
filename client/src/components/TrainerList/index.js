cbd055322064bf9332d64193818419228e2ae2f9
Tsin Moua <tsinmoua@gmail.com>
2020-12-08 15:05:12 -0600

    added redirects and changed images from random

--- client/src/components/TrainerList/index.js
+++ client/src/components/TrainerList/index.js
@@ -9,7 +9,7 @@ function TrainerList(props) {
                 return (
                     <a href={`/trainerProfile/${trainer.UserId}`} className="list-group-item list-group-item-action" key={index} traineruserid={trainer.UserId}>
                         <div className="col-12">
-                            <img src={`https://randomuser.me/api/portraits/men/${Math.floor(Math.random() * 50)}.jpg`} alt="Trainer Img" style={{ borderRadius: "50%", width: "200px", height: "200px" }} />
+                            <img src={`https://randomuser.me/api/portraits/women/${index}.jpg`} alt="Trainer Img" style={{ borderRadius: "50%", width: "200px", height: "200px" }} />
                             <h1>{trainer.firstName} {trainer.lastName}</h1>
                             <h3>{trainer.credentials}</h3>
                             <p>{trainer.about}</p>
@@ -30,7 +30,7 @@ function ClientList(props) {
                 return (
                     <a href={`/clientProfile/${client.UserId}`} className="list-group-item list-group-item-action" key={index} clientuserid={client.UserId}>
                         <div className="col-12">
-                            <img src={`https://randomuser.me/api/portraits/men/${Math.floor(Math.random() * 50)}.jpg`} alt="Client Img" style={{ borderRadius: "50%", width: "200px", height: "200px" }} />
+                            <img src={`https://randomuser.me/api/portraits/men/${index}.jpg`} alt="Client Img" style={{ borderRadius: "50%", width: "200px", height: "200px" }} />
                             <h1>{client.firstName} {client.lastName}</h1>
                         </div>
                     </a>
--- client/src/pages/trainerside/trainerprofile.js
+++ client/src/pages/trainerside/trainerprofile.js
@@ -55,7 +55,7 @@ function TrainerProfile() {
 
                     <h1 className="justify-content-center">{trainer.firstName} {trainer.lastName}</h1>
                     <div className="img-container justify-content-center">
-                    <img src={`https://randomuser.me/api/portraits/men/${Math.floor(Math.random() * 50)}.jpg`} alt="Trainer Img" style={{borderRadius: "50%", width: "200px", height: "200px"}}/>
+                    <img src={`https://randomuser.me/api/portraits/men/${id}.jpg`} alt="Trainer Img" style={{borderRadius: "50%", width: "200px", height: "200px"}}/>
                     </div>
                     <br />
                     <div>
