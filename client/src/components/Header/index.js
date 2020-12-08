546feb54522a48a05136579dbfff1c3dd74d577f
Shuelong Moua <moua_shuelong@yahoo.com>
2020-12-08 01:48:04 -0600

    updated header with logo

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,9 +1,11 @@
 import React from "react";
 import "./style.css";
+import logo from "../../assets/images/logo.png";
 
 function Header() {
     return (
-        <div className="container" id="machli-header">
+        <div id="machli-header">
+            <img alt="tiger" src={logo} width="120" height="120" />
             <h1>Machli Fitness</h1>
         </div>
     );
